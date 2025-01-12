a4d2e70e882a52707230688e16cbecb6a8f4dea7
Shivang Sharma <sharmashivang1999@gmail.com>
2025-01-12 18:37:21 +0530

    feat: signup api created with dummy data

--- src/index.js
+++ src/index.js
@@ -2,12 +2,31 @@ import express from "express";
 import "dotenv/config";
 import { connectDB } from "./db/config.js";
 import cors from "cors";
+import { User } from "./models/user/user.models.js";
 
 const app = express();
 
 app.use(cors());
 
 const port = process.env.PORT || 6001;
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    name: "Shivang Sharma",
+    email: "shivang@gmail.com",
+    password: "shivang@123",
+    phoneNumber: "9876543210",
+    age: 25,
+    gender: "M",
+  });
+
+  try {
+    await user.save();
+    res.send("user created successfully");
+  } catch (error) {
+    res.status(400).send("Error in creating user");
+  }
+});
 
 connectDB()
   .then(() => {
