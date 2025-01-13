18b5b66458c57a304ca9524027fb9d1d8c5969bd
Shivang Sharma <sharmashivang1999@gmail.com>
2025-01-13 22:36:44 +0530

    feat: feed and user apis added

--- src/index.js
+++ src/index.js
@@ -7,18 +7,34 @@ import { User } from "./models/user/user.models.js";
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 const port = process.env.PORT || 6001;
 
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (!users.length) res.status(404).send("No user found");
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Error finding the user");
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+
+  try {
+    const user = await User.find({ email: userEmail });
+    if (!user.length) res.send("No user found");
+    res.send(user);
+  } catch (error) {
+    res.status(400).send("Error finding the user");
+  }
+});
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    name: "Shivang Sharma",
-    email: "shivang@gmail.com",
-    password: "shivang@123",
-    phoneNumber: "9876543210",
-    age: 25,
-    gender: "M",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
