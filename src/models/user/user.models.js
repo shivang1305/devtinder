d8695fdc54677ae9cb19045acf87c55fe37ce2d4
Shivang Sharma <sharmashivang1999@gmail.com>
2025-01-22 12:45:06 +0530

    feat: custom validation func added

--- src/models/user/user.models.js
+++ src/models/user/user.models.js
@@ -29,6 +29,10 @@ const userSchema = new mongoose.Schema(
     gender: {
       type: String,
       required: true,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value))
+          throw new Error("Gender data is not valid");
+      },
     },
   },
   { timestamps: true }
