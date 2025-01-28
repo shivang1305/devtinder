a3b5f15a6f29db5e66da4f0dc9c459d361e0acdd
Shivang Sharma <sharmashivang1999@gmail.com>
2025-01-28 17:28:36 +0530

    feat: allowed updates enum & default img url added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,12 @@
 export const DB_NAME = "devtinder";
+
+export const DEFAULT_IMAGE_URL =
+  "https://w7.pngwing.com/pngs/205/731/png-transparent-default-avatar-thumbnail.png";
+
+export const ALLOWED_UPATES = [
+  "photoUrl",
+  "age",
+  "gender",
+  "phoneNumber",
+  "name",
+];
