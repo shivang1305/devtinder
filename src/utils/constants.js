c0f4c6b7e2f602ce93865a1c332af7f97713904d
Shivang Sharma <sharmashivang1999@gmail.com>
2025-01-28 18:28:11 +0530

    feat: allowed gender values added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,8 @@ export const DB_NAME = "devtinder";
 export const DEFAULT_IMAGE_URL =
   "https://w7.pngwing.com/pngs/205/731/png-transparent-default-avatar-thumbnail.png";
 
+export const ALLOWED_GENDER_VALUES = ["M", "MALE", "F", "FEMALE", "O", "OTHER"];
+
 export const ALLOWED_UPATES = [
   "photoUrl",
   "age",
