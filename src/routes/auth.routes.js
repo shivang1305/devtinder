96c05521fd029ff3b9624f5bf1736e29eada073b
Shivang Sharma <sharmashivang1999@gmail.com>
2025-06-07 00:05:38 +0530

    feat: auth api routes added

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { userLogin, userSignup } from "../controllers/user.controller.js";
+import {
+  userLoginValidator,
+  userSignupValidator,
+} from "../validators/user.validators.js";
+
+const router = express.Router();
+
+router.post("/signup", userSignupValidator, userSignup);
+router.post("/login", userLoginValidator, userLogin);
+
+export default router;
