a37f6c5e1ce362d1b5ae211961a8140b1130a911
Shivang Sharma <sharmashivang1999@gmail.com>
2025-06-07 18:09:19 +0530

    chore: cookie-parser package added

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import express from "express";
 import "dotenv/config";
 import { connectDB } from "./db/config.js";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 const port = process.env.PORT || 6001;
 
 import authRoutes from "./routes/auth.routes.js";
+import profileRoutes from "./routes/profile.routes.js";
 
 app.use("/auth", authRoutes);
+app.use("/profile", profileRoutes);
 
 connectDB()
   .then(() => {
