ef0bb8a53d54110b6802bc28f3c7901e2ac4a2d4
Shivang Sharma <sharmashivang1999@gmail.com>
2025-07-21 23:16:02 +0530

    feat: verification code helper func created

---
+++ src/utils/helper.js
@@ -0,0 +1,18 @@
+import { transporter } from "../config/mailer";
+
+export const generateVerificationCode = () =>
+  Math.floor(100000 + Math.random() * 900000).toString();
+
+export const sendVerificationEmail = async (email, code) => {
+  await transporter.sendMail({
+    from: `"devtinder 👨‍💻❤️" <${process.env.EMAIL_USER}>`,
+    to: email,
+    subject: "Verify your email - Devtinder",
+    html: `
+      <h2>Welcome to devtinder 🚀</h2>
+      <p>Your verification code is:</p>
+      <h1 style="color:#e91e63;">${code}</h1>
+      <p>This code is valid for 15 minutes. Please do not share it with anyone.</p>
+    `,
+  });
+};
