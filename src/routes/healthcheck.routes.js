0b8aee1545696e4811677fb13acd52811aaa2562
Shivang Sharma <sharmashivang1999@gmail.com>
2025-07-23 16:44:30 +0530

    fix: import typo

--- src/routes/healthcheck.routes.js
+++ src/routes/healthcheck.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { healthcheck } from "../controllers/healthcheck.controller";
+import { healthcheck } from "../controllers/healthcheck.controller.js";
 
 const router = express.Router();
 
