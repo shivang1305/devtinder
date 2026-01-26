1f01c503d8f4f587ac62935206fefd37a27d8df2
Shivang Sharma <sharmashivang1999@gmail.com>
2026-01-26 18:00:08 +0530

    user collections response modified to user data only

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import { ConnectionRequest } from "../models/connectionRequest/connectionRequest.model.js";
+import { USER_PUBLIC_FIELDS } from "../utils/constants.js";
 
 const getPendingConnectionRequests = async (req, res) => {
   try {
@@ -7,16 +8,7 @@ const getPendingConnectionRequests = async (req, res) => {
     const connectionRequests = await ConnectionRequest.find({
       toUserId: loggedInUserId,
       status: "like", // to get only pending requests
-    }).populate("fromUserId", [
-      "firstName",
-      "lastName",
-      "age",
-      "gender",
-      "photoUrl",
-      "bio",
-      "interests",
-      "gallery",
-    ]);
+    }).populate("fromUserId", USER_PUBLIC_FIELDS);
 
     res.status(200).json({
       message: "Connection requests fetched successfully",
@@ -37,12 +29,28 @@ const getUserConnections = async (req, res) => {
         { toUserId: loggedInUserId, status: "accept" },
       ],
     })
-      .populate("fromUserId", ["firstName", "lastName"])
-      .populate("toUserId", ["firstName", "lastName"]);
+      .populate("fromUserId", USER_PUBLIC_FIELDS)
+      .populate("toUserId", USER_PUBLIC_FIELDS);
+
+    const userConnections = connections.map((connection) => {
+      if (connection.fromUserId._id.toString() === loggedInUserId) {
+        return {
+          _id: connection._id,
+          user: connection.toUserId,
+          connectedAt: connection.updatedAt,
+        };
+      } else {
+        return {
+          _id: connection._id,
+          user: connection.fromUserId,
+          connectedAt: connection.updatedAt,
+        };
+      }
+    });
 
     res.status(200).json({
       message: "User connections fetched successfully",
-      connections: connections,
+      connections: userConnections,
     });
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" + error.message });
