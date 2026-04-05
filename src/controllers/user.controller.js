aa53d6cd74142e0f77a4d5017032636c83c2b32e
Shivang Sharma <sharmashivang1999@gmail.com>
2026-04-05 16:29:04 +0530

    feed api added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import { ConnectionRequest } from "../models/connectionRequest/connectionRequest.model.js";
+import { User } from "../models/user/user.models.js";
 import { USER_PUBLIC_FIELDS } from "../utils/constants.js";
 
 const getPendingConnectionRequests = async (req, res) => {
@@ -57,4 +58,42 @@ const getUserConnections = async (req, res) => {
   }
 };
 
-export { getPendingConnectionRequests, getUserConnections };
+const userFeed = async (req, res) => {
+  // Implementation for user feed goes here
+  // all the profiles suggestions should be based on user preferences (gender, age, location, interests, etc.)
+  // user should not see his own profile in the feed
+  // user should not see profiles of users he has already liked/disliked/connected with
+
+  const page = parseInt(req.query.page) || 1;
+  let limit = parseInt(req.query.limit) || 10;
+  limit = Math.min(limit, 50); // to prevent too large limit
+  const skip = (page - 1) * limit;
+
+  const connections = await ConnectionRequest.find({
+    $or: [{ fromUserId: req.user.id }, { toUserId: req.user.id }],
+  }).select(["fromUserId", "toUserId"]);
+
+  const hideUsersFromFeed = new Set();
+
+  connections.forEach((connection) => {
+    hideUsersFromFeed.add(connection.fromUserId.toString());
+    hideUsersFromFeed.add(connection.toUserId.toString());
+  });
+
+  const usersToShowInFeed = await User.find({
+    $and: [
+      { _id: { $nin: Array.from(hideUsersFromFeed) } },
+      { _id: { $ne: req.user.id } },
+    ],
+  })
+    .select(USER_PUBLIC_FIELDS)
+    .limit(limit)
+    .skip(skip);
+
+  res.status(200).json({
+    data: usersToShowInFeed,
+    message: "User feed fetched successfully",
+  });
+};
+
+export { getPendingConnectionRequests, getUserConnections, userFeed };
